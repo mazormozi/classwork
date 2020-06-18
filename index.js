a29c0012fe6c8342dfb4bcc8125ebd9cda2f67ea
maormozi <maormozi7@gmail.com>
2020-06-18 18:27:37 +0300

    new function

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function factFunction(num) {
+    let result = 1;
+    for (let i =1 ; i<=num ; i++){
+        result +=i ;
+    }
+    return result;
+}
+
+document.writeln(factFunction(4))
+document.writeln(factFunction(6))
+
